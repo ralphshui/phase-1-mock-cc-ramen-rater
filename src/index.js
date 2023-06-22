118c89422e42c31f91edec50a423ea2f30723a4a
Ralph Shui <rskc.2112@gmail.com>
2023-06-21 20:08:39 -0400

    completing core deliverables in ramen challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-// write your code here
+let ramenData;
+
+const ramenNavImages = document.getElementById('ramen-menu');
+
+const ramenName = document.querySelector('h2');
+const ramenRestaurant = document.querySelector('.restaurant');
+const ramenImage = document.querySelector('.detail-image');
+const rating = document.getElementById('rating-display');
+const comment = document.getElementById('comment-display');
+
+const form = document.getElementById('new-ramen');
+
+const newName = document.getElementById('new-name');
+const newRestaurant = document.getElementById('new-restaurant');
+const newRamenImage = document.getElementById('new-image');
+const newRating = document.getElementById('new-rating');
+const newComment = document.getElementById('new-comment');
+
+fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(data => {
+        ramenData = data;
+//      ramenData[0];
+
+        renderPics();
+        renderDetails();
+        createNewRamen();
+    });
+
+const renderPics = () => {
+    ramenData.forEach((ramen) => {
+        let image = document.createElement('img');
+        image.src = ramen.image;
+        ramenNavImages.append(image);
+
+        image.addEventListener('click', () => {
+            ramenData = ramen;
+            renderDetails();
+        })
+    })
+}
+
+const renderDetails = () => {
+    ramenName.textContent = ramenData.name;
+    ramenRestaurant.textContent = ramenData.restaurant;
+    ramenImage.src = ramenData.image;
+    ramenImage.alt = `${ramenData.name} Picture`;
+    rating.textContent = ramenData.rating;
+    comment.textContent = ramenData.comment;
+}
+
+const createNewRamen = () => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+               
+        let newRamen = {
+            'id': ramenData.length+1,
+            'name': newName.value,
+            'restaurant': newRestaurant.value,
+            'image': newRamenImage.value,
+            'rating': newRating.value,
+            'comment': newComment.value,
+        }
+
+        let newImg = document.createElement('img');
+        newImg.src = newRamen.image;
+        ramenNavImages.append(newImg);
+
+        ramenData.push(newRamen)
+        
+    form.reset();
+    })
+}
